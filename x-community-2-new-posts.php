1a421923d56f7f3793c44aebd86a660966e15037
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-21 14:14:34 +0800

    finished widget

---
+++ x-community-2-new-posts.php
@@ -0,0 +1,68 @@
+<?php
+widget_css();
+
+$icon_url = widget_data_url( $widget_config['code'], 'icon' );
+
+$file_headers = @get_headers($icon_url);
+
+if( $file_headers[0] == 'HTTP/1.1 404 Not Found') {
+    $icon_url = x::url()."/widget/".$widget_config['name']."/img/new.gif";
+}
+
+if( $widget_config['title'] ) $title = $widget_config['title'];
+else $title = "새로 등록된 글";
+
+if( $widget_config['no'] ) $limit = $widget_config['no'];
+else $limit = 15;
+
+$posts = g::posts(
+	array(
+		'domain'=>etc::domain(),				
+		'limit'=> $limit
+	)
+);
+?>
+
+<div class='new-posts'>
+	 <div class='title'>
+		<img class='new-post-icon' src='<?=$icon_url?>' />
+		<?=$title?>
+	 </div>
+	 <?php
+	 $dot_url = x::url()."/widget/".$widget_config['name']."/img/dot.gif";
+	 
+	 if ( $posts ) {
+		foreach ( $posts as $p ) {
+			$url = G5_BBS_URL."/board.php?bo_table=$p[bo_table]&wr_id=$p[wr_id]";
+			$new_subject = conv_subject( $p['wr_subject'], 14, '...');					
+			echo "
+					<div class='row'>
+						<img class='dot-icon' src='$dot_url'/><a href='$url'>$new_subject</a>
+					</div>
+			";
+
+		}
+	  }
+	  else {?>
+		<div class='row'>
+			<img class='dot-icon' src='<?=$dot_url?>'/>
+			<a href='http://www.philgo.net/bbs/board.php?bo_table=help&wr_id=5'>사이트 만들기 안내</a>
+		</div>
+		<div class='row'>
+			<img class='dot-icon' src='<?=$dot_url?>'/>
+			<a href='http://www.philgo.net/bbs/board.php?bo_table=help&wr_id=4'>블로그 만들기</a>
+		</div>
+		<div class='row'>
+			<img class='dot-icon' src='<?=$dot_url?>'/>
+			<a href='http://www.philgo.net/bbs/board.php?bo_table=help&wr_id=3'>커뮤니티 사이트 만들기</a>
+		</div>
+		<div class='row'>
+			<img class='dot-icon' src='<?=$dot_url?>'/>
+			<a href='http://www.philgo.net/bbs/board.php?bo_table=help&wr_id=2'>여행사 사이트 만들기</a>
+		</div>
+		<div class='row'>
+			<img class='dot-icon' src='<?=$dot_url?>'/>
+			<a href='http://www.philgo.net/bbs/board.php?bo_table=help&wr_id=1'>(모바일)홈페이지, 스마트폰 앱</a>
+		</div>
+	<?}?>
+</div>
